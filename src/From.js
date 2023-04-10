78450fb36a61cb5f0869cc93d7a081dc1b614545
Deepak Prajapati <86870784+deepakprajapati2112@users.noreply.github.com>
2023-04-10 19:39:41 +0530

    first commit

---
+++ src/From.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextInput from './TextInput.js';
+
+class Form extends React.Component {
+    render() {
+        return(
+            <div><TextInput /></div>
+        )
+    }
+}
+
+export default Form;
